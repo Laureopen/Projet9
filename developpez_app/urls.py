981e36de628387dc31066af725cf813f6195e9a6
Laureopen <laurebretagne56@orange.fr>
2025-04-14 11:51:57 +0200

    Mise à jour de Django

--- developpez_app/urls.py
+++ developpez_app/urls.py
@@ -22,5 +22,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('users.urls')),
     path('reviews/', include('reviews.urls')),
-
 ]
--- reviews/urls.py
+++ reviews/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='reviews-home'),
-    path('tickets/', views.ticket_list, name='ticket-list'),
+    path('tickets/', views.ticket_list, name='ticket-list')
 
 ]
