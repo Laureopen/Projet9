b6ca91873e5fadeaffe572db9271f3f8634197f1
laurence <laur.grenouille@orange.fr>
2025-05-10 15:43:52 +0200

    Mise à jour django

--- developpez_app/settings.py
+++ developpez_app/settings.py
@@ -105,11 +105,11 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'fr'
+USE_I18N = True
+USE_L10N = True
 
 TIME_ZONE = 'UTC'
-
-USE_I18N = True
 
 USE_TZ = True
 
